aa18fcf026b86f5cc250f51bf48463b422d62977
laszloke <37046661+laszloke@users.noreply.github.com>
2018-03-27 17:35:34 +0300

    lint stuff

--- app.js
+++ app.js
@@ -1,68 +1,64 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const APIRouter = require('./routes/API/index')
+const users = require('./routes/api/users')
 
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const APIRouter = require('./routes/API/index');
-const users = require('./routes/api/users');
-
-
-const app = express();
+const app = express()
 // Connect Mongoose
-mongoose.connect('mongodb://localhost/musiclist');
+mongoose.connect('mongodb://localhost/musiclist')
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 app.use(require('express-session')({
   secret: 'any random string can go here',
   resave: false,
   saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/API', APIRouter);
-app.use('/api/users', users);
-
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/API', APIRouter)
+app.use('/api/users', users)
 
 // Configure Passport
-const User = require('./models/user');
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+const User = require('./models/user')
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 // catch 404 and forward to error handler
-app.use((req, res, next)=> {
-  next(createError(404));
-});
+app.use((req, res, next) => {
+  next(createError(404))
+})
 
 // error handler
-app.use((err, req, res, next)=> {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- routes/API/users.js
+++ routes/API/users.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // Import our User model
-const User = require('../../models/user');
+const User = require('../../models/user')
 
 // GET User List.
 router.get('/list', (req, res, next) => {
-    // Find all matching users, which in this case is all of 'em
-    User.find((err, users) => {
-        if (err) {
-          // if something's broken, send an error
-            res.send(err);
-        }
-          // Otherwise, send the array of users.
-        res.json(users);
-      });    
-});
+  // Find all matching users, which in this case is all of 'em
+  User.find((err, users) => {
+    if (err) {
+      // if something's broken, send an error
+      res.send(err)
+    }
+    // Otherwise, send the array of users.
+    res.json(users)
+  })   
+})
 
-module.exports = router;
+module.exports = router
