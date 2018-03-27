3208c4ea02f4526f25223f8c73164e093e17425f
laszloke <37046661+laszloke@users.noreply.github.com>
2018-03-27 15:55:39 +0300

    unit testing added

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const APIRouter = require('./routes/API/index')
+const APIRouter = require('./routes/API/index');
+const users = require('./routes/api/users');
+
 
 const app = express();
+// Connect Mongoose
+mongoose.connect('mongodb://localhost/musiclist');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,11 +26,28 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'any random string can go here',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/API', APIRouter);
+app.use('/api/users', users);
+
+
+// Configure Passport
+const User = require('./models/user');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use((req, res, next)=> {
