29255059a2968c4c6ae40343b7e50b5d1bac6c1d
laszloke <laszloke@gmail.com>
2018-03-27 23:53:30 +0300

    react running as in example

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
     "extends": "standard",
     "plugins": [ "jest" ],
     "env": {
-      "jest/globals": true
+      "jest/globals": true,
+      "browser": true,
+      "node": true,
     },
     "rules": {
       "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
---
+++ src/testcomponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestComponent () {
+  return (
+    <div>
+      <h1>React Marci Test Component</h1>
+    </div>
+  )
+}
